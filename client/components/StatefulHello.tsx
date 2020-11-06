e91c7c9bba72136efbbd5ad5f5cc7d6b097195b1
Cesar Roman <cesardroman18@gmail.com>
2020-11-06 12:23:36 -0500

    Added one stateful class practice file

--- client/components/StatefulHello.tsx
+++ client/components/StatefulHello.tsx
@@ -14,4 +14,36 @@ class Hello extends React.Component<Props, State> {
     super(props);
     this.state = { currentEnthusiasm: props.enthusiasmLevel || 1 };
   }
+
+  updateEnthusiasm(currentEnthusiasm: number) {
+    this.setState({ currentEnthusiasm });
+  }
+
+  getExclamationMarks(numChars: number) {
+    return Array(numChars + 1).join('!');
+  }
+
+  onIncrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+
+  onDecrement = () => this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+
+  render() {
+    const { name } = this.props;
+
+    if (this.state.currentEnthusiasm <= 0) {
+      throw new Error('You could be a little more enthusiastic. :D');
+    }
+
+    return (
+      <div>
+        <div>
+          Hello {name + this.getExclamationMarks(this.state.currentEnthusiasm)}
+        </div>
+        <button onClick={this.onIncrement}>+</button>
+        <button onClick={this.onDecrement}>-</button>
+      </div>
+    );
+  }
 }
+
+export default Hello;
